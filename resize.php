9f22c09a9ea0d4beb72e45fb286bd8229804d1db
pbourke <pbourke@gmail.com>
2010-05-12 06:37:14 -0700

    Cleaned up the script in preparation for doing aspect-ratio consistent scaling.

--- resize.php
+++ resize.php
@@ -7,21 +7,28 @@
 // Increase the memory limit so that large files can be processed. A ~5M image requires
 // >30M of memory per script invocation
 ini_set("memory_limit", "64M");
-// // Content type
+
+// Get the script arguments - source url and maximum dimensions of the scaled image
+$src_url = $_GET['u'];
+$max_width = $_GET['w'];
+$max_height = $_GET['h'];
+// Todo: accept expected content type
+
+// Load the original image (most likely over the network) 
+$src_image = imagecreatefromjpeg($src_url);
+
+// Compute the width and height of the scaled image, preserving aspect ratio
+$src_width = imagesx($src_image);
+$src_height = imagesy($src_image);
+
+// create a target for the scaled image
+$scaled_image = imagecreatetruecolor($max_width, $max_height);
+
+// Resize
+imagecopyresized($scaled_image, $src_image, 0, 0, 0, 0, $max_width, $max_height, $src_width, $src_height);
+
+// Output
+// todo: output expected content type
 header('Content-type: image/jpeg');
-//
-// // Get new sizes
-$width = $_GET['w'];
-$height = $_GET['h'];
-$filename = $_GET['u'];
-//
-// // Load
-$thumb = imagecreatetruecolor($width, $height);
-$source = imagecreatefromjpeg($filename);
-//
-// // Resize
-imagecopyresized($thumb, $source, 0, 0, 0, 0, $width, $height, imagesx($source), imagesy($source));
-//
-// // Output
-imagejpeg($thumb);
+imagejpeg($scaled_image);
 ?>
